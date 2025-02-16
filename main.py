aaf569ff09c28995e387753b8eae4d84a79ba8f4
Krishna Kanth Latya <simplycodesmart@gmail.com>
2025-02-17 01:16:42 +0530

    first commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ music_file = f"{random.randint(1, 11)}.mp3"
 files_to_download = [music_file, 'font.ttf', 'bg.png']
 
 def run():
-    download_files(scopes=DRIVE_SCOPE,files_to_download=None)
+    download_files(scopes=DRIVE_SCOPE,files_to_download=files_to_download)
     text = "Your Custom Text Here"
     uploaded_image = text_on_background(text, 'bg.png', 'font.ttf', 'output_image.png')
     if uploaded_image:
