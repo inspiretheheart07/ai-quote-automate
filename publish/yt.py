26829f0ff2ec1e57c9e0bc6625c6f69cc1657e61
Krishna Kanth Latya <simplycodesmart@gmail.com>
2025-02-18 22:44:08 +0530

    first commit

--- publish/yt.py
+++ publish/yt.py
@@ -34,7 +34,24 @@ RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+   %s
+
+with information from the API Console
+https://console.cloud.google.com/
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
 VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
+
 
 def get_authenticated_service():
         # Directly use oauth.json for OAuth authentication
