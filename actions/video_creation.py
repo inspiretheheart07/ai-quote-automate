b6c83c976b36957802bb4a1924bc589805872489
Krishna Kanth Latya <simplycodesmart@gmail.com>
2025-02-22 00:07:42 +0530

    first commit

--- actions/video_creation.py
+++ actions/video_creation.py
@@ -5,5 +5,5 @@ def create_video_with_music(image_path, music_file):
     image_clip = ImageClip(image_path, duration=55)
     image_clip = image_clip.set_audio(audio_clip)
     video_path = 'output_video.mp4'
-    image_clip.write_videofile(video_path, fps=24)
+    image_clip.write_videofile(video_path, fps=24,codec='libx264', audio_codec='aac')
     return video_path
